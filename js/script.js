462a8640bed14eb76f4508959a26353b4994b382
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-05-26 23:28:51 +0300

    Выполнил задание 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// в практике набор переменных можно так объявлять?
+// или лучше перед каждой переменной писать let/const
+
+let money = 5,
+  income = 5,
+  ddExpenses = 'func',
+  deposit = [],
+  mission = {},
+  period = 0;
+
+console.log('Hello world');
+
+alert('Hello world');
