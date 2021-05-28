e038a677dabb8ce3e273bfd56fe9e06d486b265a
dmitrii_zaitsev <zaycevdi@gmail.com>
2021-05-28 20:21:22 +0300

    Выполнил задание 3

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 'use strict'
-const money = 45000; // Доход за месяц
-const income = 5000;  // дополнительный доход
-const addExpenses = 'Учёба, Коммуналка, Транспорт'; // дополнительные расходы через запятую
-const deposit = false;
-const mission = 500000; //сумма которую нужно накопить
-const period = 10; // период от 1 до 12 (месяцев)
+// Доход за месяц
+const money = +prompt('Ваш месячный доход?', '45000');
+// дополнительный доход
+const income = 'фриланс';
+// дополнительные расходы через запятую
+const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+  'игры, проездной, кредит');
+
+const deposit = confirm('Есть ли у вас депозит в банке?');
+//сумма которую нужно накопить
+const mission = 500000;
+//Обязательные статьи расходов
+const expenses1 = prompt('Введите обязательную статью расходов?', 'Квартплата');
+const amount1 = +prompt('Во сколько это обойдется?', '10000');
+const expenses2 = prompt('Введите обязательную статью расходов?', 'Питание');
+const amount2 = +prompt('Во сколько это обойдется?', '15000');
+
+// дневной бюджет
+const budgetDay = Math.floor(money / 30); 
+
+// Вычисляем бюджет на месяц
+const budgetMonth = money - (amount1 + amount2);
+
+//за сколько месяцев будет достигнута цель
+const period = Math.ceil(mission / budgetMonth);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -12,9 +31,18 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} рублей`);
+console.log(budgetDay);
+console.log(`Бюджет на месяц: ${budgetMonth}`);
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay >= 0) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что то пошло не так');
+}
 
 const arrExpenses = addExpenses.toLowerCase().split(',');
 console.log(arrExpenses.map(item => item.trim()));
-
-const budgetDay = money / 30; // дневной бюджет
-console.log(budgetDay);
