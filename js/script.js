a1ffb145119f0c94fe943b44517c42cb459edb36
Dmitriy <43322960+ZaycevDmitriy@users.noreply.github.com>
2021-06-19 22:33:18 +0300

    Изменил метод resert

--- js/script.js
+++ js/script.js
@@ -194,18 +194,18 @@ AppData.prototype.blockInput = function () {
 };
 
 AppData.prototype.reset = function () {
-  this.income = {};
-  this.incomeMonth = 0;
-  this.addIncome = [];
-  this.expenses = {};
-  this.addExpenses = [];
-  this.deposit = false;
-  this.percentDeposit = 0;
-  this.moneyDeposit = 0;
-  this.budget = 0;
-  this.budgetDay = 0;
-  this.budgetMonth = 0;
-  this.expensesMonth = 0;
+  Object.assign(this, new AppData);
+  // this.incomeMonth = 0;
+  // this.addIncome = [];
+  // this.expenses = {};
+  // this.addExpenses = [];
+  // this.deposit = false;
+  // this.percentDeposit = 0;
+  // this.moneyDeposit = 0;
+  // this.budget = 0;
+  // this.budgetDay = 0;
+  // this.budgetMonth = 0;
+  // this.expensesMonth = 0;
 
   document.querySelectorAll('.expenses-items').forEach((item, index) => {
     if (index > 0) {
